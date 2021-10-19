0cb5217b78424c9cfeea04099fc42ac1b23e9c9f
sgtWalker <rzrsdiego@outlook.com>
2021-10-19 20:46:25 -0300

    First controller and razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,10 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Salles Web MVC App from C# Course";
+            ViewData["Professor"] = "Nélio Alves";
+            ViewData["Student"] = "Diego Araujo";
+            ViewData["LinkedIn"] = "https://www.linkedin.com/in/diegomarquesaraujo/";
 
             return View();
         }

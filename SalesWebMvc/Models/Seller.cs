24ed627911994ddc4703462986d450fc8239835c
Diego Araujo <diego.araujo@totvs.com.br>
2022-01-25 20:32:51 -0300

    Foreign key not null (referential integrity)

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -14,6 +14,7 @@ namespace SalesWebMvc.Models
         public double BaseSalary { get; set; }
         //associação (Seller possui 1 Department)
         public Department Department { get; set; }
+        public int DepartmentId { get; set; }
         //associação (Seller possui várias SalesRecord)
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
         #endregion
